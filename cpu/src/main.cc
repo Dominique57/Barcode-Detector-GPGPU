974cf44db56c11bdd9a7ad3542ed2d9176a4bd7a
Dominique MICHEL <dominique.michel@epita.fr>
2021-05-19 23:04:26 +0200

    Add c++ template

---
+++ cpu/src/main.cc
@@ -0,0 +1,40 @@
+#include "main.hh"
+
+namespace po = boost::program_options;
+
+int main(int argc, char** argv)
+{
+    try {
+        auto const& desc = define_options();
+        auto const& vm = parse_options(desc, argc, argv);
+        run(desc, vm);
+    } catch (po::error &e) {
+        std::cerr << "[ERROR] " << e.what() << std::endl;
+        return 2;
+    }
+    return 0;
+}
+
+po::options_description define_options()
+{
+    po::options_description desc("Allowed options");
+    desc.add_options()
+        ("help,h", "show usage");
+    return desc;
+}
+
+po::variables_map parse_options(const po::options_description& desc, int argc,
+        char** argv)
+{
+    po::variables_map vm;
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);
+
+    return vm;
+}
+
+void run(const po::options_description& desc, const po::variables_map& vm)
+{
+    if (vm.count("help"))
+        std::cout << desc << std::endl;
+}
